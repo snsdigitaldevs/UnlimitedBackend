48852307057ae62aa72fff7eeb0da3820be134b7
zfxiao <luckygirlalisa@gmail.com>
2018-04-09 10:28:55 +0800

    Generate spring boot base project.

---
+++ src/main/java/com/simonschuster/pimsleur/unlimited/UnlimitedApplication.java
@@ -0,0 +1,12 @@
+package com.simonschuster.pimsleur.unlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlimitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlimitedApplication.class, args);
+	}
+}

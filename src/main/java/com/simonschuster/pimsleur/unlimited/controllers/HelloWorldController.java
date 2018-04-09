e203fe384cf563e4f89eae6ed960480aee355f46
zfxiao <luckygirlalisa@gmail.com>
2018-04-09 11:21:25 +0800

    Add sample controller and it's API test.

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/HelloWorldController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 }
--- src/test/java/com/simonschuster/pimsleur/unlimited/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/simonschuster/pimsleur/unlimited/controllers/HelloWorldControllerTest.java
@@ -1,5 +1,30 @@
-import static org.junit.Assert.*;
+package com.simonschuster.pimsleur.unlimited.controllers;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    public void getHelloResponseCorrectly() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
 }

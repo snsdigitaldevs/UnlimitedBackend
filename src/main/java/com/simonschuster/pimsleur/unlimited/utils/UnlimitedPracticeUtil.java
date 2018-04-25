88cf9dab12ef3af2ac3802f62f24f4fb87d3f567
wsj <scglwsj@163.com>
2018-04-25 15:40:20 +0800

    [wsj] temporary storage

---
+++ src/main/java/com/simonschuster/pimsleur/unlimited/utils/UnlimitedPracticeUtil.java
@@ -0,0 +1,11 @@
+package com.simonschuster.pimsleur.unlimited.utils;
+
+import com.simonschuster.pimsleur.unlimited.data.dto.practices.AvailablePractices;
+
+import java.util.HashMap;
+
+public class UnlimitedPracticeUtil {
+    public static AvailablePractices getAvailablePractices() {
+        return null;
+    }
+}

72edf5648eae26ef73020535c612985db7374ea0
pfcui <cuipengfei2008@gmail.com>
2018-04-25 15:14:37 +0800

    [cpf] 161 csv examples

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/AvailablePracticesController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/AvailablePracticesController.java
@@ -1,10 +1,10 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
-import com.simonschuster.pimsleur.unlimited.data.dto.customerInfo.CustomerInfoDTO;
 import com.simonschuster.pimsleur.unlimited.data.dto.practices.AvailablePractices;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 //this api tells you what kind of practices are available for each lesson inside a course
 

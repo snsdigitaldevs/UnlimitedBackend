44f7f7c447d850be7533f7fb0bfd55f4e5b92501
zfxiao <luckygirlalisa@gmail.com>
2018-05-01 10:57:15 +0800

    [Zhanfei][PIm-33] Add test for url encode.

---
+++ src/test/java/com/simonschuster/pimsleur/unlimited/utils/UrlUtilTest.java
@@ -0,0 +1,16 @@
+package com.simonschuster.pimsleur.unlimited.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UrlUtilTest {
+    @Test
+    public void shouldEncodeUrlCorrectly() throws Exception {
+        String puOriginUrl = "https://install.pimsleurunlimited.com/staging_n/common/mandarinchinese/Mandarin Chinese I/audio/9781442394872_Mandarin_Chinese1_U01_Lesson.mp3";
+
+        String encodedUrl = UrlUtil.encodeUrl("https://install.pimsleurunlimited.com/staging_n/common", puOriginUrl);
+
+        assertEquals("https://install.pimsleurunlimited.com/staging_n/common/mandarinchinese/Mandarin+Chinese+I/audio/9781442394872_Mandarin_Chinese1_U01_Lesson.mp3", encodedUrl);
+    }
+}

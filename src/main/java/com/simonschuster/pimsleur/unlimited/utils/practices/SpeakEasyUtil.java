8215534f657a617901d5db0fb2f7882acc89092a
pfcui <cuipengfei2008@gmail.com>
2018-05-03 09:39:43 +0800

    [cpf] 94 method signature for speak easy

---
+++ src/main/java/com/simonschuster/pimsleur/unlimited/utils/practices/SpeakEasyUtil.java
@@ -0,0 +1,11 @@
+package com.simonschuster.pimsleur.unlimited.utils.practices;
+
+import com.simonschuster.pimsleur.unlimited.data.dto.practices.PracticesInUnit;
+
+import java.util.List;
+
+public class SpeakEasyUtil {
+    public List<PracticesInUnit> csvToSpeakEasies(String csvUrl) {
+        return null;
+    }
+}

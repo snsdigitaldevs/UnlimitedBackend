847e12712f765f7cc66c40c740baf6acafe4a60f
pfcui <cuipengfei2008@gmail.com>
2018-05-07 16:10:04 +0800

    [cpf] 118 Create POJO models for PCM free lessons

---
+++ src/main/java/com/simonschuster/pimsleur/unlimited/data/edt/freeLessons/PCMFreeLessons.java
@@ -0,0 +1,40 @@
+
+package com.simonschuster.pimsleur.unlimited.data.edt.freeLessons;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "result_data",
+        "result_code"
+})
+public class PCMFreeLessons {
+
+    @JsonProperty("result_data")
+    private ResultData resultData;
+    @JsonProperty("result_code")
+    private long resultCode;
+
+    @JsonProperty("result_data")
+    public ResultData getResultData() {
+        return resultData;
+    }
+
+    @JsonProperty("result_data")
+    public void setResultData(ResultData resultData) {
+        this.resultData = resultData;
+    }
+
+    @JsonProperty("result_code")
+    public long getResultCode() {
+        return resultCode;
+    }
+
+    @JsonProperty("result_code")
+    public void setResultCode(long resultCode) {
+        this.resultCode = resultCode;
+    }
+
+}

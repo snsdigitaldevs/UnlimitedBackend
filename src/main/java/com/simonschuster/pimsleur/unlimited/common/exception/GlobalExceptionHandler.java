b4a49d62ee814a897b85dc7b3f2bd2c3ddd1807a
kwyang <kwyang@thoughtworks.com>
2018-05-07 15:42:38 +0800

    [kwyang] fix bug :get lessons with title prefix lesson and unit

--- src/main/java/com/simonschuster/pimsleur/unlimited/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,13 @@
 package com.simonschuster.pimsleur.unlimited.common.exception;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.stream.Collectors;
 
 import static com.simonschuster.pimsleur.unlimited.common.exception.ErrorResponse.of;
 import static org.springframework.core.annotation.AnnotatedElementUtils.findMergedAnnotation;

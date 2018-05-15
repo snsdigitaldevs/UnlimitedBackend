8dde0451fcc79b42a28399adda5aa1f49e557c0e
kwyang <kwyang@thoughtworks.com>
2018-05-15 15:36:00 +0800

    [kwyang][PIM-125] customer management catch error

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
+import com.simonschuster.pimsleur.unlimited.common.exception.ParamInvalidException;
 import com.simonschuster.pimsleur.unlimited.data.edt.customerinfo.CustomerInfo;
 import com.simonschuster.pimsleur.unlimited.services.customer.CustomerInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +17,32 @@ public class CustomerController {
                                        @PathVariable String appUserId,
                                        @RequestParam String token,
                                        @RequestParam String name) {
-        return customerInfoService.update(customerId, appUserId, name, token);
+        CustomerInfo customerInfo = customerInfoService.update(customerId, appUserId, name, token);
+        if (customerInfo.getResult_code() != 1) {
+            throw new ParamInvalidException("invalid parameters");
+        }
+        return customerInfo;
     }
 
     @PostMapping(value = "customer/{customerId}")
     public CustomerInfo createUserInfo(@PathVariable String customerId,
                                        @RequestParam String token,
                                        @RequestParam String name) {
-        return customerInfoService.create(customerId, name, token);
+        CustomerInfo customerInfo = customerInfoService.create(customerId, name, token);
+        if (customerInfo.getResult_code() != 1) {
+            throw new ParamInvalidException("invalid parameters");
+        }
+        return customerInfo;
     }
 
     @DeleteMapping(value = "customer/{customerId}/appUserIds/{appUserId}")
     public CustomerInfo deleteUserInfo(@PathVariable String customerId,
                                        @RequestParam String token,
                                        @PathVariable String appUserId) {
-        return customerInfoService.delete(customerId, appUserId, token);
+        CustomerInfo customerInfo = customerInfoService.delete(customerId, appUserId, token);
+        if (customerInfo.getResult_code() != 1) {
+            throw new ParamInvalidException("invalid parameters");
+        }
+        return customerInfo;
     }
 }

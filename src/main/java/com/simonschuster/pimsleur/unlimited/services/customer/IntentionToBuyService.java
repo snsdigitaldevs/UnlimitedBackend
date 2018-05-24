bd916927e4d2b01cab4adf70e2dec5bdefaecc2f
wsj <scglwsj@163.com>
2018-05-24 16:01:25 +0800

    [wsj] 126 add app id

--- src/main/java/com/simonschuster/pimsleur/unlimited/services/customer/IntentionToBuyService.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/services/customer/IntentionToBuyService.java
@@ -16,13 +16,24 @@ public class IntentionToBuyService {
     @Autowired
     private ApplicationConfiguration config;
 
+    private static final String APP_ID_IOS = "com.thoughtworks.pimsleur.unlimited.inapppurchase";
+    private static final String APP_ID_ANDROID = "com.thoughtworks.pimsleur.unlimited.inapppurchase";
+    // TODO: make sure each app_id in iOS and Android
+
     public void intentionToBuy(String customerId, String isbn, String storeDomain) {
         String url = config.getProperty("edt.api.intentionToBuy.url");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+        String appId = "";
+        if (storeDomain.contains("android")) {
+            appId = APP_ID_ANDROID;
+        } else if (storeDomain.contains("ios")) {
+            appId = APP_ID_IOS;
+        }
+
         HttpEntity<String> entity = new HttpEntity<>(String.format(config.getProperty("edt.api.intentionToBuy.parameters"),
-                isbn, storeDomain, customerId), headers);
+                isbn, storeDomain, customerId, appId), headers);
         CodeOnlyResposeEDT intentionToBuyResponse = postToEdt(entity, url, CodeOnlyResposeEDT.class);
         if (!intentionToBuyResponse.getResultCode().equals(1)) {
             throw new PimsleurException("intention to buy failed!");

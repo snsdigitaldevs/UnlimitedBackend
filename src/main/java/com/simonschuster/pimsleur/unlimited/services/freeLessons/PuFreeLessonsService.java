e432f894cad36108f3087eef62ffb4ee1448e425
pfcui <cuipengfei2008@gmail.com>
2018-06-12 16:26:28 -0700

    CPF Call pcm products api with different parameters in dev and qa env to get new pcm free isbns. for uat and prod keep the same as before

--- src/main/java/com/simonschuster/pimsleur/unlimited/services/freeLessons/PuFreeLessonsService.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/services/freeLessons/PuFreeLessonsService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static com.simonschuster.pimsleur.unlimited.utils.HardCodedProductsUtil.PU_FREE_LESSONS;
-import static java.util.Arrays.asList;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.concat;
@@ -15,25 +14,13 @@ import static java.util.stream.Stream.concat;
 @Service
 public class PuFreeLessonsService {
 
-    private static List<String> pcmFreeLessonsToIgnore = asList(
-            "9781442307674",//French
-            "9781442308923",//German
-            "9781442314931",//Italian
-            "9781442313583",//Spanish
-            "9781442312463",//Russian
-            "9781442374140",//Brazilian Portuguese
-            "9781442316034",//Mandarin Chinese
-            "9781442328686",//ESL Spanish
-            "9781442310223"//Japanese
-    );
-
     public List<AvailableProductDto> mergePuWithPcmFreeLessons(List<AvailableProductDto> pcmFreeLessons) {
-        Stream<AvailableProductDto> pcmFreeLessonsWithout9PuCourses =
+        Stream<AvailableProductDto> pcmFreeLessonsWithoutOverlappingPU =
                 pcmFreeLessons.stream()
-                        .filter(pcmFreeLesson -> pcmFreeLessonsToIgnore.stream()
-                                .noneMatch(ignored -> ignored.equals(pcmFreeLesson.getProductCode())));
+                        .filter(pcmFree ->
+                                PU_FREE_LESSONS.stream().noneMatch(puFree -> puFree.isSameLang(pcmFree)));
 
-        return concat(pcmFreeLessonsWithout9PuCourses, PU_FREE_LESSONS.stream())
+        return concat(pcmFreeLessonsWithoutOverlappingPU, PU_FREE_LESSONS.stream())
                 .sorted(comparing(AvailableProductDto::getLanguageName))
                 .collect(toList());
     }

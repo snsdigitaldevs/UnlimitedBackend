e8a826e07f6cfbed13127cc99c669aefe48e5504
pfcui <cuipengfei2008@gmail.com>
2018-06-25 16:35:01 +0800

    [bug] use pims2.0 as default app id instead of empty string

--- src/main/java/com/simonschuster/pimsleur/unlimited/utils/InAppPurchaseUtil.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/utils/InAppPurchaseUtil.java
@@ -6,7 +6,7 @@ public class InAppPurchaseUtil {
     // TODO: make sure each app_id in iOS and Android
 
     public static String getAppId(String storeDomain) {
-        String appId = "";
+        String appId = "pims2.0";
         if (storeDomain.contains("android")) {
             appId = APP_ID_ANDROID;
         } else if (storeDomain.contains("ios")) {

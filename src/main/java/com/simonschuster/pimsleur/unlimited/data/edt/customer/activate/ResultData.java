611acff15a854027f99cce1d055f997a0f9e11a8
pfcui <cuipengfei2008@gmail.com>
2018-06-29 10:10:16 +0800

    [data diversity] the token arr field's type is different for different people. since we don't use it anyways, just remove it from our model

--- src/main/java/com/simonschuster/pimsleur/unlimited/data/edt/customer/activate/ResultData.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/data/edt/customer/activate/ResultData.java
@@ -6,21 +6,16 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.simonschuster.pimsleur.unlimited.data.edt.customer.ProductActivation;
 import com.simonschuster.pimsleur.unlimited.data.edt.customer.Registrant;
 
-import java.util.Map;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "registrant",
-        "activation",
-        "tokenArr"
+        "activation"
 })
 public class ResultData {
     @JsonProperty("registrant")
     private Registrant registrant;
     @JsonProperty("activation")
     private ProductActivation activation;
-    @JsonProperty("tokenArr")
-    private Map<String, String> tokenArr;
 
     @JsonProperty("registrant")
     public Registrant getRegistrant() {
@@ -40,15 +35,5 @@ public class ResultData {
     @JsonProperty("activation")
     public void setActivation(ProductActivation activation) {
         this.activation = activation;
-    }
-
-    @JsonProperty("tokenArr")
-    public Map<String, String> getTokenArr() {
-        return tokenArr;
-    }
-
-    @JsonProperty("tokenArr")
-    public void setTokenArr(Map<String, String> tokenArr) {
-        this.tokenArr = tokenArr;
     }
 }

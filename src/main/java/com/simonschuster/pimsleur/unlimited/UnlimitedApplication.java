893611ddfe944b1cfb0d1becf92601e72e4c8cf3
pfcui <cuipengfei2008@gmail.com>
2018-07-03 17:08:47 +0800

    [log] access log to syslog

--- src/main/java/com/simonschuster/pimsleur/unlimited/UnlimitedApplication.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/UnlimitedApplication.java
@@ -3,7 +3,9 @@ package com.simonschuster.pimsleur.unlimited;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.simonschuster.pimsleur")
@@ -13,5 +15,15 @@ public class UnlimitedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnlimitedApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
     }
 }

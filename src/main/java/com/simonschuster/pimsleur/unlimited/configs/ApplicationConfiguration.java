9caaab9ea720120b5a73dea5ceb0dcede02fb117
zfxiao <luckygirlalisa@gmail.com>
2018-07-09 15:57:35 +0800

    [Zhanfei][PIM-533] Add cross origin resource sharing of alexa image for alexa web app.

--- src/main/java/com/simonschuster/pimsleur/unlimited/configs/ApplicationConfiguration.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/configs/ApplicationConfiguration.java
@@ -2,8 +2,12 @@ package com.simonschuster.pimsleur.unlimited.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ConfigurationProperties
@@ -22,5 +26,17 @@ public class ApplicationConfiguration {
 
     public String getProperty(String propertyName){
         return env.getProperty(propertyName);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/alexa/alexa_image.png")
+                        .allowedOrigins("https://ask-ifr-download.s3.amazonaws.com")
+                        .allowedMethods("GET");
+            }
+        };
     }
 }

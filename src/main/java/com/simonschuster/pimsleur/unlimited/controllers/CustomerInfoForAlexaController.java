24ead49c4e58c8ca50457333440a70a52be09977
htao <htao@thoughtworks.com>
2018-07-26 10:25:32 +0800

    [taohui] #TO-41 fix bug: alex need check product code when sync progress

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerInfoForAlexaController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerInfoForAlexaController.java
@@ -1,7 +1,8 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
+import com.simonschuster.pimsleur.unlimited.data.dto.availableProducts.AvailableProductsDto;
 import com.simonschuster.pimsleur.unlimited.data.dto.customerInfo.CustomerInfoDTO;
-import com.simonschuster.pimsleur.unlimited.services.CustomerInfoConverterForAlexa;
+import com.simonschuster.pimsleur.unlimited.services.availableProducts.AvailableProductsService;
 import com.simonschuster.pimsleur.unlimited.services.customer.EDTCustomerInfoService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.simonschuster.pimsleur.alexa.StoreDomainUtil.ALEXA_STORE_DOMAIN;
 
@@ -22,7 +25,7 @@ public class CustomerInfoForAlexaController {
     private EDTCustomerInfoService edtCustomerInfoService;
 
     @Autowired
-    private CustomerInfoConverterForAlexa customerInfoConverterForAlexa;
+    private AvailableProductsService availableProductsService;
 
     @ApiOperation(value = "Get customer info for Alexa",
             notes = "Customer info includes isbns of purchased and expanded pcm and pu products, " +
@@ -36,6 +39,8 @@ public class CustomerInfoForAlexaController {
     public CustomerInfoDTO getCustomerInfo(@RequestParam(value = "sub") String sub)
             throws IOException {
         //Kelly K will handle email missing for Alexa.
+//        AggregatedCustomerInfo customerInfo = edtCustomerInfoService.getCustomerInfos(sub, ALEXA_STORE_DOMAIN, "");
+//        CustomerInfoDTO customerInfoDTO = customerInfo.toDto();
         CustomerInfoDTO customerInfoDTO = edtCustomerInfoService.getCustomerInfoDTO(sub, ALEXA_STORE_DOMAIN, "");
         customerInfoDTO.getProductActivations().forEach(activation -> { String productCode = activation.getProductCode();
             if(activation.getChildProductCodesString() != null){
@@ -43,11 +48,16 @@ public class CustomerInfoForAlexaController {
                 String[] childProductCodeArray = childProductCodes.split(",");
                 if(customerInfoDTO.getUnlimitedProductCodes().contains(productCode)){
                     customerInfoDTO.getUnlimitedProductCodes().addAll(Arrays.asList(childProductCodeArray));
-                }else if(customerInfoDTO.getPcmProductCodes().contains(productCode)){
-                    customerInfoDTO.getPcmProductCodes().addAll(Arrays.asList(childProductCodeArray));
                 }
             }
         });
+
+        AvailableProductsDto availableProducts = availableProductsService.getAvailableProducts(sub, "", ALEXA_STORE_DOMAIN);
+        List<String> purchasedPCMProductCodes = availableProducts.getPurchasedProducts()
+                .stream().filter(product -> product.isPuProduct() == false)
+                .map(product -> product.getProductCode())
+                .collect(Collectors.toList());
+        customerInfoDTO.getPcmProductCodes().addAll(purchasedPCMProductCodes);
         return customerInfoDTO;
     }
 }

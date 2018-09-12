230bab09b7142f92bfb60908de663d5c7a20acba
htao <htao@thoughtworks.com>
2018-09-12 10:28:17 +0800

    [taohui] refactor: use the same PostToEDT method when request to EDT

--- src/main/java/com/simonschuster/pimsleur/unlimited/services/course/PUCourseInfoService.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/services/course/PUCourseInfoService.java
@@ -5,19 +5,14 @@ import com.simonschuster.pimsleur.unlimited.configs.ApplicationConfiguration;
 import com.simonschuster.pimsleur.unlimited.data.edt.productinfo.AggregatedProductInfo;
 import com.simonschuster.pimsleur.unlimited.data.edt.productinfo.PuProductInfo;
 import com.simonschuster.pimsleur.unlimited.services.AppIdService;
+import com.simonschuster.pimsleur.unlimited.utils.EDTRequestUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import static java.util.Arrays.asList;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.TEXT_HTML;
 
 @Service
 public class PUCourseInfoService {
@@ -58,9 +53,6 @@ public class PUCourseInfoService {
      * @return
      */
     private PuProductInfo getProductInfoFromPu(String productCode, String storeDomain) {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(asList(TEXT_HTML, APPLICATION_JSON));
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
@@ -68,12 +60,10 @@ public class PUCourseInfoService {
         HttpEntity<String> requestEntity = new HttpEntity<>(
                 String.format(config.getApiParameter("unlimitedProductInfoDefaultParameters"), productCode, appId),
                 headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(converter);
-
         String productInfoApiUrl = config.getProperty("edt.api.productInfoApiUrl");
-        return restTemplate.postForObject(productInfoApiUrl, requestEntity, PuProductInfo.class);
-    }
 
+        PuProductInfo puProductInfo = EDTRequestUtil.postToEdt(requestEntity, productInfoApiUrl, PuProductInfo.class);
+
+        return puProductInfo;
+    }
 }

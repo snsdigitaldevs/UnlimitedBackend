e222f3624e1d7ead817fab725e2cd7f50ad65897
Yunlei Cai <ylcai@cnylcai-2.local>
2018-09-28 18:00:46 +0800

    [#N/A][Cai Yunlei & caopeng ]fix can not find current mediaitem progress,then null exception.
    (cherry picked from commit 801984dd4500bac3306c892e9219a9a846abbfe8)

--- src/main/java/com/simonschuster/pimsleur/unlimited/utils/PCMProgressConverter.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/utils/PCMProgressConverter.java
@@ -6,6 +6,9 @@ import com.simonschuster.pimsleur.unlimited.data.edt.syncState.UserAppStateDatum
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.groupingBy;
@@ -18,7 +21,7 @@ public class PCMProgressConverter {
 
     private static final String CURRENT_MEDIA_SET_HISTORY_ID = "currentMediaSetHistoryId";
     private static final String CURRENT_MEDIA_ITEM_HISTORY_ID = "currentMediaItemHistoryId";
-    public static final int PRODUCT_CODE_LENGTH = 13;
+    private static final int PRODUCT_CODE_LENGTH = 13;
 
     private static List<String> keyWordsToExtract =
             asList(AUDIO_TRACK_COMPLETE, LAST_ACCESS_DATE, LAST_AUDIO_POS_MILLIS);
@@ -30,6 +33,7 @@ public class PCMProgressConverter {
                 .collect(groupingBy(UserAppStateDatum::idPartOfKey)).values().stream()
                 .map(group -> groupToDTO(group, currentMediaItemProgressDto))
                 .collect(toList());
+
     }
 
     private static pcmProgressParserDto findCurrentMediaItem(List<UserAppStateDatum> edtProgresses) {
@@ -71,24 +75,41 @@ public class PCMProgressConverter {
 
     private static void extractProductAndMediaItem(ProgressDTO progressDTO, String key, pcmProgressParserDto currentMediaItemProgress) {
         //com.edt.models::MediaItemHistory_17892 9781 4423 1603 4 40339#lastAudioPosMillis
-        if (key.contains(currentMediaItemProgress.getProductCode()) && key.contains(currentMediaItemProgress.getMediaItem())) {
-            progressDTO.setCurrent(true);
-        }
-
         String s = key.split("#")[0];
-        int customerIdIndex = s.indexOf(currentMediaItemProgress.getCustomerId());
-        int productCodeIndex = customerIdIndex + currentMediaItemProgress.getCustomerId().length();
-        int mediaItemIndex = productCodeIndex + PRODUCT_CODE_LENGTH;
-        String code = s.substring(productCodeIndex, mediaItemIndex);
-        String mediaItem = s.substring(mediaItemIndex);
-
-        progressDTO.setProductCode(code);
-        progressDTO.setMediaItemId(Integer.parseInt(mediaItem));
+        if (currentMediaItemProgress != null) {
+            if (key.contains(currentMediaItemProgress.getProductCode()) && key.contains(currentMediaItemProgress.getMediaItem())) {
+                progressDTO.setCurrent(true);
+            }
+            int customerIdIndex = s.indexOf(currentMediaItemProgress.getCustomerId());
+            int productCodeIndex = customerIdIndex + currentMediaItemProgress.getCustomerId().length();
+            int mediaItemIndex = productCodeIndex + PRODUCT_CODE_LENGTH;
+            String code = s.substring(productCodeIndex, mediaItemIndex);
+            String mediaItem = s.substring(mediaItemIndex);
+            progressDTO.setProductCode(code);
+            progressDTO.setMediaItemId(Integer.parseInt(mediaItem));
+        }else {
+            /*com.edt.models::MediaItemHistory_68277978150821796170464*/
+            String[] splitByUnderline = s.split("_");
+            if (splitByUnderline.length == 2) {
+                String totalCode = splitByUnderline[1];
+                String regex = "(\\d{4,})(978\\d{10})(\\d+)";
+                Pattern p = Pattern.compile(regex);
+                Matcher m = p.matcher(totalCode);
+                while (m.find()) {
+                    String productCode = m.group(2);
+                    String mediaItem = m.group(3);
+                    progressDTO.setProductCode(productCode);
+                    progressDTO.setMediaItemId(Integer.parseInt(mediaItem));
+                }
+            }
+        }
     }
+
 
     private static boolean hasKeyWord(UserAppStateDatum progress) {
         boolean keyDoesNotHaveNullString = !progress.getKey().contains("null");
         return keyDoesNotHaveNullString &&
                 keyWordsToExtract.stream().anyMatch((keyWord) -> progress.getKey().contains(keyWord));
     }
+
 }

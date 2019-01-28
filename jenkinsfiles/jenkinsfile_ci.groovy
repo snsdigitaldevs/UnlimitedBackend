13c84a290908e50cefe585724f5af3406b4cdd9a
taohui <htao@thoughtworks.com>
2019-01-28 17:56:37 +0800

    [taohui] remove QA env as it is not used in mobile side

--- jenkinsfiles/jenkinsfile_ci.groovy
+++ jenkinsfiles/jenkinsfile_ci.groovy
@@ -36,18 +36,6 @@ pipeline {
             }
         }
 
-        stage("Deploy to QA") {
-            agent any
-            steps {
-                echo "Deploy to QA"
-                script {
-                    def config = readProperties file: 'jenkinsfiles/config/config.properties'
-                    def hostnames = config.QA_UnlimitedBackend_HostName.split(",")
-                    deploy(hostnames, "qa")
-                }
-            }
-        }
-
         stage("Deploy to UAT") {
             steps {
                 script {

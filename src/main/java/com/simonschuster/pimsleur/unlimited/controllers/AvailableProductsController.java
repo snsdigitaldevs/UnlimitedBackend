14b68187b12ebdca7d83089b3910fe38d469ccc0
taohui <htao@thoughtworks.com>
2019-02-14 20:55:35 +0800

    [taohui] #235 when get available products, if ISBN format is other format1, replace it with base ISBN

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/AvailableProductsController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/AvailableProductsController.java
@@ -1,7 +1,9 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
 import com.simonschuster.pimsleur.unlimited.data.dto.availableProducts.AvailableProductsDto;
+import com.simonschuster.pimsleur.unlimited.data.dto.promotions.PurchaseMapping;
 import com.simonschuster.pimsleur.unlimited.services.availableProducts.AvailableProductsService;
+import com.simonschuster.pimsleur.unlimited.services.promotions.PurchaseMappingService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,23 @@ public class AvailableProductsController {
     @Autowired
     private AvailableProductsService availableProductsService;
 
+    @Autowired
+    private PurchaseMappingService purchaseMappingService;
+
     @ApiOperation(value = "All products a customer can choose to learn(includes purchased and free)")
     @RequestMapping(value = "/availableProducts", method = RequestMethod.GET)
     public AvailableProductsDto getAvailableProducts(@RequestParam(value = "sub", required = false) String sub,
                                                      @RequestParam(value = "email", required = false) String email,
                                                      @RequestParam(value = "storeDomain", required = false) String storeDomain) {
-        return availableProductsService.getAvailableProducts(sub, email, storeDomain);
+        AvailableProductsDto availableProducts = availableProductsService.getAvailableProducts(sub, email, storeDomain);
+        availableProducts.getPurchasedProducts().forEach(item -> {
+            String productCode = item.getProductCode();
+            PurchaseMapping withOtherFormatAs = purchaseMappingService.findISBNWithOtherFormatAs(productCode);
+            if(withOtherFormatAs != null){
+                item.setProductCode(withOtherFormatAs.getISBN());
+            }
+        });
+        return availableProducts;
     }
 
 }

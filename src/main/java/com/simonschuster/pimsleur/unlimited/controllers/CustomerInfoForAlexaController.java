7cbfbe02d80c993dcb9be7900f100d938032c435
taohui <htao@thoughtworks.com>
2019-02-25 17:08:01 +0800

    [taohui] remove comment line for alexa get customer info API

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerInfoForAlexaController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/CustomerInfoForAlexaController.java
@@ -1,8 +1,6 @@
 package com.simonschuster.pimsleur.unlimited.controllers;
 
-import com.simonschuster.pimsleur.unlimited.data.dto.availableProducts.AvailableProductsDto;
 import com.simonschuster.pimsleur.unlimited.data.dto.customerInfo.CustomerInfoDTO;
-import com.simonschuster.pimsleur.unlimited.services.availableProducts.AvailableProductsService;
 import com.simonschuster.pimsleur.unlimited.services.customer.EDTCustomerInfoService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.simonschuster.pimsleur.alexa.StoreDomainUtil.ALEXA_STORE_DOMAIN;
 
@@ -23,9 +18,6 @@ public class CustomerInfoForAlexaController {
 
     @Autowired
     private EDTCustomerInfoService edtCustomerInfoService;
-
-    @Autowired
-    private AvailableProductsService availableProductsService;
 
     @ApiOperation(value = "Get customer info for Alexa",
             notes = "Customer info includes isbns of purchased and expanded pcm and pu products, " +
@@ -40,22 +32,6 @@ public class CustomerInfoForAlexaController {
             throws IOException {
         //Kelly K will handle email missing for Alexa.
         CustomerInfoDTO customerInfoDTO = edtCustomerInfoService.getCustomerInfoDTO(sub, ALEXA_STORE_DOMAIN, "");
-//        customerInfoDTO.getProductActivations().forEach(activation -> { String productCode = activation.getProductCode();
-//            if(activation.getChildProductCodesString() != null){
-//                String childProductCodes = activation.getChildProductCodesString();
-//                String[] childProductCodeArray = childProductCodes.split(",");
-//                if(customerInfoDTO.getUnlimitedProductCodes().contains(productCode)){
-//                    customerInfoDTO.getUnlimitedProductCodes().addAll(Arrays.asList(childProductCodeArray));
-//                }
-//            }
-//        });
-
-//        AvailableProductsDto availableProducts = availableProductsService.getAvailableProducts(sub, "", ALEXA_STORE_DOMAIN);
-//        List<String> purchasedPCMProductCodes = availableProducts.getPurchasedProducts()
-//                .stream().filter(product -> product.isPuProduct() == false)
-//                .map(product -> product.getProductCode())
-//                .collect(Collectors.toList());
-//        customerInfoDTO.getPcmProductCodes().addAll(purchasedPCMProductCodes);
         return customerInfoDTO;
     }
 }

afcec9f76fa0e94dc9391d4ce3cf1621f4e0e5a6
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2019-05-27 16:14:56 +0800

    [jinghui] #TO-297  add Norwegian Prem free lesson level1

--- src/main/java/com/simonschuster/pimsleur/unlimited/utils/HardCodedProductsUtil.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/utils/HardCodedProductsUtil.java
@@ -9,15 +9,16 @@ import static java.util.stream.Collectors.toList;
 
 public class HardCodedProductsUtil {
     public static List<AvailableProductDto> PU_FREE_LESSONS = asList(
-            new AvailableProductDto("French","French", "9781508279709", true),
-            new AvailableProductDto("German","German", "9781508243298", true),
-            new AvailableProductDto("Italian","Italian", "9781508243304", true),
-            new AvailableProductDto("Spanish","Spanish", "9781508283737", true),//!!
-            new AvailableProductDto("Russian","Russian", "9781508243335", true),//!!skills
-            new AvailableProductDto("Portuguese Brazilian","Portuguese Brazilian", "9781508243342", true),
-            new AvailableProductDto("Chinese Mandarin","Chinese Mandarin", "9781508243328", true),//!!skills
-            new AvailableProductDto("English for Spanish Speakers","ESL Spanish", "9781508243359", true),
-            new AvailableProductDto("Japanese","Japanese", "9781508243311", true)
+            new AvailableProductDto("French", "French", "9781508279709", true),
+            new AvailableProductDto("German", "German", "9781508243298", true),
+            new AvailableProductDto("Italian", "Italian", "9781508243304", true),
+            new AvailableProductDto("Spanish", "Spanish", "9781508283737", true),//!!
+            new AvailableProductDto("Russian", "Russian", "9781508243335", true),//!!skills
+            new AvailableProductDto("Portuguese Brazilian", "Portuguese Brazilian", "9781508243342", true),
+            new AvailableProductDto("Chinese Mandarin", "Chinese Mandarin", "9781508243328", true),//!!skills
+            new AvailableProductDto("English for Spanish Speakers", "ESL Spanish", "9781508243359", true),
+            new AvailableProductDto("Japanese", "Japanese", "9781508243311", true),
+            new AvailableProductDto("Norwegian", "Norwegian", "9781508299646", true)
     );
 
     private static final List<String> nineBigLanguageNames = PU_FREE_LESSONS.stream()

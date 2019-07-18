42052ae3a922a3017e26814d334169955a62bb1b
ya xu <yaxu@thoughtworks.com>
2019-07-18 10:44:07 +0800

    [xuya] #WD-22 add get location from ip address function

---
+++ src/main/java/com/simonschuster/pimsleur/unlimited/data/dto/price/LocationInfoDTO.java
@@ -0,0 +1,37 @@
+package com.simonschuster.pimsleur.unlimited.data.dto.price;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
+public class LocationInfoDTO {
+    private String name;
+
+    private String ip;
+
+    public LocationInfoDTO(String ip) {
+        this.ip = ip;
+    }
+
+    public LocationInfoDTO(String name, String ip) {
+        this.name = name;
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}

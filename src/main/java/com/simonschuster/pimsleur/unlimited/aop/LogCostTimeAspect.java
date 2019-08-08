5890b46a144581f30dcb08aadf867809c6aa783c
Kangkang Hu <kangkang.hu@thoughtworks.com>
2019-08-08 17:50:44 +0800

    [kangkang] # log record update

--- src/main/java/com/simonschuster/pimsleur/unlimited/aop/LogCostTimeAspect.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/aop/LogCostTimeAspect.java
@@ -18,21 +18,16 @@ public class LogCostTimeAspect {
   private final Logger LOG = LoggerFactory.getLogger(LogCostTimeAspect.class);
 
   @Around("@annotation(com.simonschuster.pimsleur.unlimited.aop.annotation.LogCostTime)")
-  public Object aroundPrintCostTime(ProceedingJoinPoint joinPoint) {
+  public Object aroundPrintCostTime(ProceedingJoinPoint joinPoint) throws Throwable {
     MethodSignature signature = (MethodSignature) joinPoint.getSignature(); //方法签名
     LogCostTime logCostTime = signature.getMethod().getAnnotation(LogCostTime.class);//从签名注解中获取注解内容配置项
     String className = signature.getDeclaringType().getSimpleName();
     String methodName = StringUtils.isEmpty(logCostTime.value()) ? signature.getName() : logCostTime.value();
-    try {
       long startTimeMillis = System.currentTimeMillis();
       Object result = joinPoint.proceed();
       long execTimeMillis = System.currentTimeMillis() - startTimeMillis;
       LOG.info("{}@{} execute cost : {}ms", className, methodName,
           execTimeMillis);
       return result;
-    } catch (Throwable e) {
-      LOG.error(methodName + "execute fail caused by", e);
-      throw new RuntimeException(e.getMessage());
-    }
   }
 }

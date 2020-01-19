be9642aa6768c83d3f85a666248fbf2aa4bd83e1
chi.wang <wangchi327@163.com>
2020-01-19 10:24:08 +0800

    [chiwang] rename build package name in ci

--- jenkinsfiles/jenkinsfile_only_ci.groovy
+++ jenkinsfiles/jenkinsfile_only_ci.groovy
@@ -7,8 +7,9 @@ pipeline {
                 echo "Compile"
 
                 script {
+                    env.GIT_BRANCH = sh(returnStdout: true, script: 'git symbolic-ref --short -q HEAD').trim()
                     env.GIT_REVISION = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-                    echo "${env.GIT_REVISION}"
+                    echo "${GIT_BRANCH}-${env.GIT_REVISION}"
                     checkout scm
 
                     def project_name = "UnlimitedBackend"
@@ -18,7 +19,7 @@ pipeline {
 
                     def build_package = "${WORKSPACE}/target/${build_package_name}"
                     def all_build_package_dir = "~/jenkins_build_package/${project_name}"
-                    def build_package_new_name = "${project_name}-${env.GIT_REVISION}-${BUILD_ID}.jar"
+                    def build_package_new_name = "${project_name}-${env.GIT_BRANCH}.${env.GIT_REVISION}-${BUILD_ID}.jar"
                     echo "${build_package_new_name}"
 
                     sh "mkdir -p  ${all_build_package_dir}"

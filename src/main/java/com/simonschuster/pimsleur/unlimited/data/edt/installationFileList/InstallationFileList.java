51abc3db01aeb3eabd95e64b5b7cd19ddc364c7c
chi.wang <wangchi327@163.com>
2020-02-14 09:47:21 +0800

    #P20E-53[chiwang] use endWith instead of contains

--- src/main/java/com/simonschuster/pimsleur/unlimited/data/edt/installationFileList/InstallationFileList.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/data/edt/installationFileList/InstallationFileList.java
@@ -126,6 +126,6 @@ public class InstallationFileList extends EdtResponseCode {
     }
 
     private boolean isPdfFiles(FileListItem fileListItem) {
-        return fileListItem.getPath().contains(".pdf") && fileListItem.getMimeType().equals("pdf");
+        return fileListItem.getPath().endsWith(".pdf") && fileListItem.getMimeType().equals("pdf");
     }
 }

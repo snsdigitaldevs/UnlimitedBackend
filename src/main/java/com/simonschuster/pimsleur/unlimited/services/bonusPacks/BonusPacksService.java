9588e839a639d9f38bded80cb62264b50d6f4b07
chi.wang <chi.wang@thoughtworks.com>
2020-03-06 09:34:50 +0800

    #P20E-134[chiwang] add log for get bonus pack url failed

--- src/main/java/com/simonschuster/pimsleur/unlimited/services/bonusPacks/BonusPacksService.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/services/bonusPacks/BonusPacksService.java
@@ -1,9 +1,12 @@
 package com.simonschuster.pimsleur.unlimited.services.bonusPacks;
 
 import com.simonschuster.pimsleur.unlimited.data.dto.bonusPacks.BonusPackInUnit;
+import com.simonschuster.pimsleur.unlimited.data.edt.EdtResponseCode;
 import com.simonschuster.pimsleur.unlimited.data.edt.installationFileList.InstallationFileList;
 import com.simonschuster.pimsleur.unlimited.services.InstallationFileService;
 import com.simonschuster.pimsleur.unlimited.utils.bonusPacks.BonusPackUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +17,10 @@ import java.util.List;
 @Service
 public class BonusPacksService {
 
+    private static final Logger logger = LoggerFactory.getLogger(BonusPacksService.class);
+
     @Autowired
     private InstallationFileService installationFileService;
-
 
     public List<BonusPackInUnit> getBonusPacksInUnit(String productCode, String storeDomain) throws IOException {
         List<BonusPackInUnit> bonusPackInUnits = new ArrayList<>();
@@ -29,7 +33,10 @@ public class BonusPacksService {
 
     private BonusPacksUrls getBonusPacksUrls(String productCode, String storeDomain) {
         InstallationFileList installationFileList = installationFileService.getInstallationFileList(productCode, storeDomain);
-
+        if (installationFileList.getResultCode() != EdtResponseCode.RESULT_OK) {
+            logger.warn("get bonusPack Url from edt failed, please check your input and try again");
+            return null;
+        }
         return installationFileList.getBonusPacksUrls();
     }
 }

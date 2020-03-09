d8e5f3ac649d1b1541152fa6b1e9c258d09f35a6
chi.wang <chi.wang@thoughtworks.com>
2020-03-09 15:17:43 +0800

    #P20E-108[chiwang] add swagger note for vocabulary operation APIs

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/VocabularyController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/VocabularyController.java
@@ -3,6 +3,7 @@ package com.simonschuster.pimsleur.unlimited.controllers;
 import com.simonschuster.pimsleur.unlimited.data.dto.vocabularies.VocabularyInfoBodyDTO;
 import com.simonschuster.pimsleur.unlimited.data.dto.vocabularies.VocabularyInfoResponseDTO;
 import com.simonschuster.pimsleur.unlimited.services.vocabularies.VocabularyService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +23,16 @@ public class VocabularyController {
     @Autowired
     private VocabularyService vocabularyService;
 
+    @ApiOperation(value = "save vocabulary for PU course",
+            notes = "save a vocabulary by one time for PU course")
     @PostMapping
     public VocabularyInfoResponseDTO saveVocabulary(@Valid @RequestBody VocabularyInfoBodyDTO vocabularyInfoBodyDTO,
                                                     @RequestParam(required = false) String storeDomain) {
         return vocabularyService.saveVocabularyToEdt(vocabularyInfoBodyDTO, storeDomain);
     }
 
+    @ApiOperation(value = "get vocabulary list for PU course",
+            notes = "get vocabulary list by customerId, subUserId and productCode")
     @GetMapping
     public VocabularyInfoResponseDTO getVocabularyList(@RequestParam String customerId,
                                                     @RequestParam String subUserId,
@@ -36,6 +41,8 @@ public class VocabularyController {
         return vocabularyService.getSaveVocabularyList(customerId, subUserId, productCode, storeDomain);
     }
 
+    @ApiOperation(value = "delete vocabularies for PU course",
+            notes = "delete vocabularies by customerId, subUserId, productCode and language array")
     @DeleteMapping
     public VocabularyInfoResponseDTO deleteVocabularies(@RequestParam String customerId,
                                                        @RequestParam String subUserId,

ace33377662c6c023cf8bcaad50d2847516e6f6e
chi.wang <chi.wang@thoughtworks.com>
2020-03-27 09:23:04 +0800

    [chiwang] #NA add sonar scanner in ci

--- jenkinsfiles/jenkinsfile_new_ci.groovy
+++ jenkinsfiles/jenkinsfile_new_ci.groovy
@@ -7,8 +7,7 @@ pipeline {
                 echo "Compile"
 
                 script {
-                    env.GIT_BRANCH_NAME = env.GIT_BRANCH.substring(7)
-                    echo "${env.GIT_BRANCH_NAME}"
+                    echo "${env.BRANCH_NAME}"
                     checkout scm
 
                     def project_name = "UnlimitedBackend"
@@ -18,13 +17,23 @@ pipeline {
 
                     def build_package = "${WORKSPACE}/target/${build_package_name}"
                     def all_build_package_dir = "~/jenkins_build_package/${project_name}"
-                    def build_package_new_name = "${project_name}-${env.GIT_BRANCH_NAME}-${BUILD_ID}.jar"
+                    def build_package_new_name = "${project_name}-${env.BRANCH_NAME}-${BUILD_ID}.jar"
                     echo "${build_package_new_name}"
 
                     sh "mkdir -p  ${all_build_package_dir}"
                     sh "cp ${build_package} ${all_build_package_dir}/${build_package_new_name}"
                     sh([returnStdout: true, script: "python jenkinsfiles/scripts/delHistoryPkg.py ${all_build_package_dir}"])
                     currentBuild.description = "${build_package_new_name} build success!"
+                }
+            }
+        }
+        stage('Sonar-Scan'){
+            agent any
+            steps{
+                withSonarQubeEnv('sonarqube-host') {
+                    sh '''
+                    mvn sonar:sonar -Dsonar.projectKey=UnlimitedBackend_${env.BRANCH_NAME} -Dsonar.projectName=UnlimitedBackend_${env.BRANCH_NAME}
+                    '''
                 }
             }
         }

752684763e3e7a15c9fd176294c44e96b4530d40
Jinghua Li <jinhli@thoughtworks.com>
2021-05-25 17:17:29 +0800

    [Jinghua] #TO-861 Change language name for protuguese and spanish castilian for alexa

--- src/main/java/com/simonschuster/pimsleur/unlimited/controllers/FreeLessonsController.java
+++ src/main/java/com/simonschuster/pimsleur/unlimited/controllers/FreeLessonsController.java
@@ -15,25 +15,38 @@ import java.util.List;
 public class FreeLessonsController {
     @Autowired
     private PcmFreeLessonsService pcmFreeLessonsService;
-
+    
     @Autowired
     private PuFreeLessonsService puFreeLessonsService;
-
+    
     @Autowired
     private LearnerInfoService learnerInfoService;
-
+    
     @ApiOperation(value = "Get a list of all free lessons of both PU and PCM")
-    @RequestMapping(value = "/freeLessons", method = RequestMethod.GET)
+    @GetMapping(value = "/freeLessons")
     public List<AvailableProductDto> getFreeLessons(@RequestParam(value = "storeDomain") String storeDomain) {
         List<AvailableProductDto> pcmFreeLessons = pcmFreeLessonsService.getPcmFreeLessons(storeDomain);
-        return puFreeLessonsService
-                .mergePuWithPcmFreeLessons(pcmFreeLessons);
+        final List<AvailableProductDto> availableProducts = puFreeLessonsService
+            .mergePuWithPcmFreeLessons(pcmFreeLessons);
+        return storeDomain.equals("alexa") ? changeLanguageNameForAlexa(availableProducts) : availableProducts;
     }
-
+    
     @ApiOperation(value = "Before trying free lessons, users are asked to fill in their emails. " +
-            "Call this api to tell EDT the user's email and further actions will be taken by EDT.")
-    @RequestMapping(value = "/freeLessons/learnerInfo", method = RequestMethod.POST)
+        "Call this api to tell EDT the user's email and further actions will be taken by EDT.")
+    @PostMapping(value = "/freeLessons/learnerInfo")
     public void learnerInfo(@RequestBody LearnerInfoBodyDTO learnerInfoBodyDTO) {
         learnerInfoService.LearnerInfo(learnerInfoBodyDTO);
+    }
+    
+    private List<AvailableProductDto> changeLanguageNameForAlexa(List<AvailableProductDto> availableProducts) {
+        availableProducts.forEach(product -> {
+            if (product.getProductCode().equals("9781508277293")) {
+                product.setLanguageName("Portuguese (European)");
+            }
+            if (product.getProductCode().equals("9781797129396")) {
+                product.setLanguageName("Castilian Spanish");
+            }
+        });
+        return availableProducts;
     }
 }
